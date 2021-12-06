3bccb70d58ab85d3ea36a5cc29876ab091869e54
kouaouci <karimakouaouci2803@gmail.com>
2021-12-06 22:17:42 +0100

    maj

--- Hangar.java
+++ Hangar.java
@@ -6,9 +6,9 @@ public static void main(String[] args) {
 	
 
 	Car clio = new Car("Clio", 0);
-	System.out.println(clio.doStuff());
+	System.out.println("i'm" +"" +clio.doStuff()+ "" +"and i go zoom zoom");
 	Boat elizabeth = new Boat("Queen Elizabath",0);
-	System.out.println(elizabeth.doStuff());
+	System.out.println("i'm "+ elizabeth.doStuff()+ ""+ " and i go glug glug");
 	
 
 }}
